4c22d49d623bfd5fd935365d07b503a432d35682
mrgian <mail@gian.im>
2021-10-09 20:54:30 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,79 @@ use secp256k1::*;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use crypto::ripemd160::Ripemd160;
-use hex::FromHex;
 use base58::*;
 
+use rand::Rng;
+
 fn main() {
-    let private256 = "03902e4f09664bc177fe4e090dcd9906b432b50f15fb6151984475c1c75c35b6";
-    let private_bytes = <[u8; 32]>::from_hex(private256).unwrap();
+    let private_key = random_key();
+    println!("Private Key: {:x?}", private_key);
 
+    let public_key = private_to_public_key(&private_key);
+    println!("Public Key: {:x?}", public_key);
+
+    let sha256_hashed = hash_sha256(&public_key);
+    let ripemd160_hashed = hash_ripemd160(&sha256_hashed);
+    let with_net_prefix = add_net_prefix(&ripemd160_hashed);
+
+    let first_sha256 = hash_sha256(&with_net_prefix);
+    let second_sha256 = hash_sha256(&first_sha256);
+
+    let with_checksum = add_checksum(&with_net_prefix, &second_sha256);
+    
+    let address = with_checksum.to_base58();
+    
+    println!("Public Address: {}", address);  
+}
+
+fn random_key() -> [u8; 32] {
+    return rand::thread_rng().gen::<[u8; 32]>();
+}
+
+fn private_to_public_key(private_key: &[u8; 32]) -> [u8; 33] {
     let secp = Secp256k1::new();
-    let secret_key = SecretKey::from_slice(&private_bytes).unwrap();
-    let verifying_key = PublicKey::from_secret_key(&secp, &secret_key).serialize();
+    let secret_key = SecretKey::from_slice(private_key).unwrap();
+    return PublicKey::from_secret_key(&secp, &secret_key).serialize();
+}
 
-    println!("Secret Key: {:x?}", verifying_key);
+fn hash_sha256(data: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.input(data);
+    let mut hashed: [u8; 32] = [0; 32];
+    hasher.result(&mut hashed);
 
-    let mut sha256_hasher = Sha256::new();
-    sha256_hasher.input(&verifying_key);
-    let mut sha256_hashed: [u8; 32] = [0; 32];
-    sha256_hasher.result(&mut sha256_hashed);
+    return hashed;
+}
 
-    let mut ripemd160_hasher = Ripemd160::new();
-    ripemd160_hasher.input(&sha256_hashed);
-    let mut ripemd160_hashed: [u8; 20] = [0; 20];
-    ripemd160_hasher.result(&mut ripemd160_hashed);
+fn hash_ripemd160(data: &[u8]) -> [u8; 20] {
+    let mut hasher = Ripemd160::new();
+    hasher.input(data);
+    let mut hashed: [u8; 20] = [0; 20];
+    hasher.result(&mut hashed);
 
-    println!("Hashed: {:x?}", ripemd160_hashed);
+    return hashed;
+}
 
-    let mut withnet: [u8; 21] = [0; 21];
+fn add_net_prefix(data: &[u8; 20]) -> [u8; 21] {
+    let mut with_net_prefix: [u8; 21] = [0; 21];
 
     for n in 1..21 {
-        withnet[n] = ripemd160_hashed[n-1];
+        with_net_prefix[n] = data[n-1];
     } 
 
-    println!("With Net: {:x?}", withnet);
+    return with_net_prefix;
+}
 
-
-    let mut sha1_hasher = Sha256::new();
-    sha1_hasher.input(&withnet);
-    let mut sha1_hashed: [u8; 32] = [0; 32];
-    sha1_hasher.result(&mut sha1_hashed);
-
-    let mut sha2_hasher = Sha256::new();
-    sha2_hasher.input(&sha1_hashed);
-    let mut sha2_hashed: [u8; 32] = [0; 32];
-    sha2_hasher.result(&mut sha2_hashed);
-
-    println!("2SHA: {:x?}", sha2_hashed); 
-    
-    let mut withcheck: [u8; 25] = [0; 25];
+fn add_checksum(data: &[u8; 21], full_checksum: &[u8; 32]) -> [u8; 25] {
+    let mut with_checksum: [u8; 25] = [0; 25];
 
     for n in 0..21 {
-        withcheck[n] = withnet[n];
+        with_checksum[n] = data[n];
     }
 
     for n in 0..4 {
-        withcheck[n + 21] = sha2_hashed[n];
+        with_checksum[n + 21] = full_checksum[n];
     }
 
-    println!("With Check: {:x?}", withcheck);
-
-    let public_key = withcheck.to_base58();
-    println!("Public Key: {}", public_key);
+    return with_checksum;
 }
